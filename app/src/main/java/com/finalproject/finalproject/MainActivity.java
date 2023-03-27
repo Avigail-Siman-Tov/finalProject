ad3b3ea5838b16c8043b53ae10da052ef3e9b5a9
Avigail-Siman-Tov <avigailsi123@gmail.com>
2023-03-27 17:29:47 +0300

    Initial commit

---
+++ app/src/main/java/com/finalproject/finalproject/MainActivity.java
@@ -0,0 +1,66 @@
+package com.finalproject.finalproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView userName;
+//    Button logout;
+    GoogleSignInClient gClient;
+    GoogleSignInOptions gOptions;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        logout = findViewById(R.id.logout);
+        userName = findViewById(R.id.userName);
+
+        gOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gClient = GoogleSignIn.getClient(this, gOptions);
+
+        GoogleSignInAccount gAccount = GoogleSignIn.getLastSignedInAccount(this);
+        if (gAccount != null){
+            String gName = gAccount.getDisplayName();
+            userName.setText(gName);
+        }
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                // TODO: Your application init goes here.
+                Intent mInHome = new Intent(MainActivity.this, LoginActivity.class);
+                MainActivity.this.startActivity(mInHome);
+                MainActivity.this.finish();
+            }
+        }, 3000);
+//        logout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                gClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        finish();
+//                        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+//                    }
+//                });
+//            }
+//        });
+    }
+}
