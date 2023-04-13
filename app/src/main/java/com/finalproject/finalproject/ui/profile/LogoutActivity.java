889bcff2243a6b6576cc407fcbf17b78b9dc3cc5
Avigail-Siman-Tov <avigailsi123@gmail.com>
2023-04-14 01:00:33 +0300

    logout

--- app/src/main/java/com/finalproject/finalproject/ui/profile/LogoutActivity.java
+++ app/src/main/java/com/finalproject/finalproject/ui/profile/LogoutActivity.java
@@ -20,6 +20,17 @@ public class LogoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logout);
-        FirebaseAuth.getInstance().signOut();
+        Runnable runnable=new Runnable() {
+            @Override
+            public void run() {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent=new Intent(LogoutActivity.this,LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+//
+            }
+        };
+        Handler handler=new Handler();
+        handler.postDelayed(runnable,5000);
     }
 }
